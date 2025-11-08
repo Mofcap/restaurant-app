695853230ae02e3acbbe3e09b669f3eb309c1200
Maouchi <fouad.maouchi.96@gmail.com>
2025-11-08 16:07:36 +0100

    tester  api  sans env

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:4000';
+const API_URL = import.meta.env.VITE_API_URL || 'https://restaurant-api-iznm.onrender.com';
 
 const API = axios.create({ 
   baseURL: API_URL,
