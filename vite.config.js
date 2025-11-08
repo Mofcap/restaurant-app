030d0f5c4046e00cf70ecf5a68e3a87e593aef25
Maouchi <fouad.maouchi.96@gmail.com>
2025-11-08 15:35:09 +0100

    configuration  pour render   pour allow

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173,
+    host: true
+  },
+  preview: {
+    port: process.env.PORT || 5173,
+    host: '0.0.0.0',
+    allowedHosts: [
+      '.onrender.com',
+      'localhost'
+    ],
+    strictPort: false
+  }
 })
